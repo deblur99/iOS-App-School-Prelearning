0809b1cd4ff842302b9eb7dfb34cbab465f4e17f
deblur99 <deblur99@kakao.com>
2023-04-26 11:10:58 +0900

    Initial Commit

---
+++ CoffeeInfoProvider/CoffeeInfoProviderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeInfoProviderApp.swift
+//  CoffeeInfoProvider
+//
+//  Created by 한현민 on 2023/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeInfoProviderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
